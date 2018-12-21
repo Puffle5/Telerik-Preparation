bceace39caee8c696a7599189558ca937444fc1c
Puffle5 <elena_haruno@abv.bg>
2018-12-22 00:32:44 +0200

    Added a saluting program.

---
+++ src/basic_Programming/Greeting.java
@@ -0,0 +1,12 @@
+package basic_Programming;
+import java.util.Scanner;
+
+public class Greeting
+{
+public static void main(String[] arguments)
+{
+//A simple printing program for saluting anyone:
+Scanner scanner = new Scanner(System.in);
+String name = scanner.nextLine();
+System.out.printf("Hello, %s!", name);
+}}
