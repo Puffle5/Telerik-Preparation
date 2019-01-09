ed32701b0275e1cbd3b71e7624a219b045f4339e
Puffle5 <elena_haruno@abv.bg>
2019-01-09 12:35:11 +0200

    Fixed.

--- src/conditional_Statements/Three.java
+++ src/conditional_Statements/Three.java
@@ -12,18 +12,17 @@ Double third = Double.parseDouble(scanner.nextLine());
 
 if (first >= -200 && first <= 200 && second >= -200 && second <= 200 && third >= -200 && third <= 200)
 {
-if ((first > second && first > third) || (first.equals(second) && first > third)
-|| (first.equals(third) && first > second) || (first.equals(second) && first.equals(third)))
+if (first >= second && first >= third)
 {
 if (first - (Math.floor(first)) == 0) {System.out.println(first.intValue());}
 else {System.out.println(first);}
 }
-else if ((second > first && second > third) || (second.equals(third) && second > first))
+else if (second >= first && second >= third)
 {
 if (second - (Math.floor(second)) == 0) {System.out.println(second.intValue());}
 else {System.out.println(second);}
 }
-else if (third > first && third > second)
+else if (third >= first && third >= second)
 {
 if (third - (Math.floor(third)) == 0) {System.out.println(third.intValue());}
 else {System.out.println(third);}
