493cb1f2e2260bfdcb4f234f77062463bb1744c7
Lenny <valentin805@gmail.com>
2019-01-28 19:48:35 +0200

    Telerik exam.

--- src/raiseUp/FirstTask.java
+++ src/raiseUp/FirstTask.java
@@ -1,11 +1,39 @@
 package raiseUp;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FirstTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please proceed!");
+        String input = scanner.nextLine();
+        int target = Integer.parseInt(scanner.nextLine());
+        String[] strings = input.split(", ");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++)
+        {numbers.add(Integer.parseInt(strings[i]));}
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            for (int j = 1; j <= numbers.size() - i; j++)
+            {
+                if (numbers.get(j) < numbers.get(j+1))
+                {
+                    int variable = numbers.get(j);
+                    variable = numbers.get(j+1);
+
+                }
+
+            }
+
+        }
+
+        System.out.println(numbers);
+
+
+
+                }
+
+
+            }
 
 
 
@@ -15,6 +43,3 @@ public class FirstTask {
 
 
 
-
-    }
-}
--- src/raiseUp/ThirdTask.java
+++ src/raiseUp/ThirdTask.java
@@ -1,11 +1,37 @@
 package raiseUp;
-
 import java.util.Scanner;
 
 public class ThirdTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please proceed!");
+        String input = scanner.nextLine();
+        char[] letters = input.toCharArray();
+
+        int len = letters.length;
+        int count = 0;
+
+        char res = letters[0];
+        for (int i=0; i<len; i++)
+        {
+            int cur_count = 1;
+            for (int j=i+1; j<len; j++)
+            {
+                if (letters[i] != letters[j])
+                    break;
+                cur_count++;
+                if (cur_count > count)
+                {
+                    count = cur_count;
+                    res = letters[i];
+                }
+
+
+            }
+
+
+        }
+
+    }}
 
 
 
@@ -16,5 +42,8 @@ public class ThirdTask {
 
 
 
-    }
-}
+
+
+
+
+
